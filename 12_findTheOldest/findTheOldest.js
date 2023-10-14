078daa78efe7e23bcd390e8447227bbdacecdeef
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2023-10-14 20:37:05 +0000

    Practice for JavaScript and the DOM

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+	return array.reduce((oldestPerson, currentPerson) => {
+		const oldestPersonAge = getAge(
+			oldestPerson.yearOfDeath,
+			oldestPerson.yearOfBirth
+		);
+		const currentPersonAge = getAge(
+			currentPerson.yearOfDeath,
+			currentPerson.yearOfBirth
+		);
+		return oldestPersonAge > currentPersonAge
+			? oldestPerson
+			: currentPerson;
+	});
 };
 
+const getAge = function (death, birth) {
+	if (!death) {
+		death = new Date().getFullYear();
+	}
+	return death - birth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
